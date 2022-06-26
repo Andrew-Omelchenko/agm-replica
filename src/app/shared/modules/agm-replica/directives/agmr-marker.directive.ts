7649cdd38ce402cd4a3edf6fedaf4d13e33f2905
Andrew Omelchenko <andrewom43@gmail.com>
2022-06-26 14:20:24 +0300

    [AGMR-0008] Added Google Map Marker's wrapper and manager.

--- src/app/shared/modules/agm-replica/directives/agmr-marker.directive.ts
+++ src/app/shared/modules/agm-replica/directives/agmr-marker.directive.ts
@@ -1,6 +1,18 @@
-import { ContentChildren, Directive, EventEmitter, forwardRef, Input, Output, QueryList } from '@angular/core';
+import {
+  AfterContentInit,
+  ContentChildren,
+  Directive,
+  EventEmitter,
+  forwardRef,
+  Input,
+  OnChanges,
+  OnDestroy,
+  Output,
+  QueryList,
+  SimpleChange,
+} from '@angular/core';
 import { FitBoundsAccessor } from '../accessors/fit-bounds.accessor';
-import { ReplaySubject, Subscription } from 'rxjs';
+import { Observable, ReplaySubject, Subscription } from 'rxjs';
 import { IFitBoundsDetails } from '../models/fit-bounds-details.model';
 import { MarkerManagerService } from '../services/marker-manager.service';
 import { IdGenerator } from '../utils/id-generator';
@@ -11,7 +23,7 @@ import { AgmrInfoWindowComponent } from '../views/agmr-info-window/agmr-info-win
   providers: [{ provide: FitBoundsAccessor, useExisting: forwardRef(() => AgmrMarker) }],
 })
 // tslint:disable:directive-class-suffix
-export class AgmrMarker {
+export class AgmrMarker implements OnDestroy, OnChanges, AfterContentInit, FitBoundsAccessor {
   /**
    * The latitude position of the marker.
    */
@@ -91,7 +103,7 @@ export class AgmrMarker {
   /**
    * This event emitter gets emitted when the user clicks twice on the marker.
    */
-  @Output() public markerDblClick: EventEmitter<AgmrMarker> = new EventEmitter<AgmrMarker>();
+  @Output() public markerDblClick: EventEmitter<AgmrMarker | null> = new EventEmitter<AgmrMarker | null>();
 
   /**
    * This event is fired when the user right-clicks on the marker.
@@ -139,5 +151,153 @@ export class AgmrMarker {
     this._id = IdGenerator()
       .next()
       .toString();
+  }
+
+  /* @internal */
+  public ngAfterContentInit(): void {
+    this.handleInfoWindowUpdate();
+    this.infoWindow.changes.subscribe(() => this.handleInfoWindowUpdate());
+  }
+
+  /** @internal */
+  public ngOnChanges(changes: { [key: string]: SimpleChange }): void {
+    if (typeof this.latitude === 'string') {
+      this.latitude = Number(this.latitude);
+    }
+    if (typeof this.longitude === 'string') {
+      this.longitude = Number(this.longitude);
+    }
+    if (typeof this.latitude !== 'number' || typeof this.longitude !== 'number') {
+      return;
+    }
+    if (!this.markerAddedToManger) {
+      this.markerManager.addMarker(this);
+      this.updateFitBoundsDetails();
+      this.markerAddedToManger = true;
+      this.addEventListeners();
+      return;
+    }
+    if (changes['latitude'] || changes['longitude']) {
+      this.markerManager.updateMarkerPosition(this);
+      this.updateFitBoundsDetails();
+    }
+    if (changes['title']) {
+      this.markerManager.updateTitle(this);
+    }
+    if (changes['label']) {
+      this.markerManager.updateLabel(this);
+    }
+    if (changes['draggable']) {
+      this.markerManager.updateDraggable(this);
+    }
+    if (changes['iconUrl']) {
+      this.markerManager.updateIcon(this);
+    }
+    if (changes['opacity']) {
+      this.markerManager.updateOpacity(this);
+    }
+    if (changes['visible']) {
+      this.markerManager.updateVisible(this);
+    }
+    if (changes['zIndex']) {
+      this.markerManager.updateZIndex(this);
+    }
+    if (changes['clickable']) {
+      this.markerManager.updateClickable(this);
+    }
+    if (changes['animation']) {
+      this.markerManager.updateAnimation(this);
+    }
+  }
+
+  /** @internal */
+  public ngOnDestroy(): void {
+    this.markerManager.deleteMarker(this);
+    // unsubscribe all registered observable subscriptions
+    this.observableSubscriptions.forEach((s) => s.unsubscribe());
+  }
+
+  private handleInfoWindowUpdate(): void {
+    if (this.infoWindow.length > 1) {
+      throw new Error('Expected no more than one info window.');
+    }
+    this.infoWindow.forEach((marker) => {
+      marker.hostMarker = this;
+    });
+  }
+
+  /** @internal */
+  public getFitBoundsDetails$(): Observable<IFitBoundsDetails> {
+    return this.fitBoundsDetails$.asObservable();
+  }
+
+  protected updateFitBoundsDetails(): void {
+    if (typeof this.latitude === 'number' && typeof this.longitude === 'number') {
+      this.fitBoundsDetails$.next({ latLng: { lat: this.latitude, lng: this.longitude } });
+    }
+  }
+
+  private addEventListeners(): void {
+    const cs = this.markerManager.createEventObservable('click', this).subscribe(() => {
+      if (this.openInfoWindow) {
+        this.infoWindow.forEach((infoWindow) => infoWindow.open());
+      }
+      this.markerClick.emit(this);
+    });
+    this.observableSubscriptions.push(cs);
+
+    const dcs = this.markerManager.createEventObservable<google.maps.MapMouseEvent>('dblclick', this).subscribe(() => {
+      this.markerDblClick.emit();
+    });
+    this.observableSubscriptions.push(dcs);
+
+    const rc = this.markerManager.createEventObservable<google.maps.MapMouseEvent>('rightclick', this).subscribe(() => {
+      this.markerRightClick.emit();
+    });
+    this.observableSubscriptions.push(rc);
+
+    const ds = this.markerManager.createEventObservable<google.maps.MapMouseEvent>('dragstart', this).subscribe((e) => {
+      this.dragStart.emit(e);
+    });
+    this.observableSubscriptions.push(ds);
+
+    const d = this.markerManager.createEventObservable<google.maps.MapMouseEvent>('drag', this).subscribe((e) => {
+      this.drag.emit(e);
+    });
+    this.observableSubscriptions.push(d);
+
+    const de = this.markerManager.createEventObservable<google.maps.MapMouseEvent>('dragend', this).subscribe((e) => {
+      this.dragEnd.emit(e);
+    });
+    this.observableSubscriptions.push(de);
+
+    const mover = this.markerManager
+      .createEventObservable<google.maps.MapMouseEvent>('mouseover', this)
+      .subscribe((e) => {
+        this.mouseOver.emit(e);
+      });
+    this.observableSubscriptions.push(mover);
+
+    const mout = this.markerManager
+      .createEventObservable<google.maps.MapMouseEvent>('mouseout', this)
+      .subscribe((e) => {
+        this.mouseOut.emit(e);
+      });
+    this.observableSubscriptions.push(mout);
+
+    const anChng = this.markerManager.createEventObservable<void>('animation_changed', this).subscribe(() => {
+      this.animationChange.emit(this.animation);
+    });
+    this.observableSubscriptions.push(anChng);
+  }
+
+  /** @internal */
+  public id(): string {
+    return this._id;
+  }
+
+  /** @internal */
+  public toString(): string {
+    return 'AgmrMarker-' + this._id;
   }
 }
--- src/app/shared/modules/agm-replica/services/marker-manager.service.ts
+++ src/app/shared/modules/agm-replica/services/marker-manager.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, NgZone } from '@angular/core';
-import { Observable, Observer } from 'rxjs';
+import { Observable, Observer, of } from 'rxjs';
+import { first, map, switchMap, tap } from 'rxjs/operators';
+
 import { AgmrMarker } from '../directives/agmr-marker.directive';
 import { GoogleMapsApiService } from './google-maps-api.service';
 
@@ -12,6 +14,139 @@ export class MarkerManagerService {
 
   constructor(protected mapsWrapper: GoogleMapsApiService, protected zone: NgZone) {}
 
+  public convertAnimation(
+    uiAnim: keyof typeof google.maps.Animation | undefined,
+  ): Observable<google.maps.Animation | undefined> {
+    if (!uiAnim) {
+      return of(undefined);
+    } else {
+      return this.mapsWrapper.getNativeMap().pipe(map(() => google.maps.Animation[uiAnim]));
+    }
+  }
+
+  public addMarker(marker: AgmrMarker): void {
+    this.convertAnimation(marker.animation)
+      .pipe(
+        first(),
+        map((animation) => {
+          if (marker && typeof marker?.latitude === 'number' && typeof marker?.longitude === 'number') {
+            return this.mapsWrapper.createMarker({
+              animation,
+              position: {
+                lat: marker.latitude,
+                lng: marker.longitude,
+              },
+              label: marker.label,
+              draggable: marker.draggable,
+              icon: marker.iconUrl,
+              opacity: marker.opacity,
+              visible: marker.visible,
+              zIndex: marker.zIndex,
+              title: marker.title,
+              clickable: marker.clickable,
+            });
+          }
+          return undefined;
+        }),
+      )
+      .subscribe((markerObservable) => {
+        if (markerObservable) {
+          this.markers.set(marker, markerObservable);
+        }
+      });
+  }
+
+  public deleteMarker(markerDirective: AgmrMarker): void {
+    const markerObservable = this.markers.get(markerDirective);
+    if (markerObservable) {
+      markerObservable.subscribe((m: google.maps.Marker) => {
+        this.zone.run(() => {
+          m.setMap(null);
+          this.markers.delete(markerDirective);
+        });
+      });
+    }
+  }
+
+  public updateMarkerPosition(marker: AgmrMarker): void {
+    const markerObservable = this.markers.get(marker);
+    if (markerObservable && typeof marker?.latitude === 'number' && typeof marker?.longitude === 'number') {
+      markerObservable.subscribe((m: google.maps.Marker) =>
+        m.setPosition({ lat: marker.latitude || 0, lng: marker.longitude || 0 }),
+      );
+    }
+  }
+
+  public updateTitle(marker: AgmrMarker): void {
+    const markerObservable = this.markers.get(marker);
+    if (markerObservable) {
+      markerObservable.subscribe((m: google.maps.Marker) => m.setTitle(marker?.title || null));
+    }
+  }
+
+  public updateLabel(marker: AgmrMarker): void {
+    const markerObservable = this.markers.get(marker);
+    if (markerObservable) {
+      markerObservable.subscribe((m: google.maps.Marker) => m.setLabel(marker?.label || null));
+    }
+  }
+
+  public updateDraggable(marker: AgmrMarker): void {
+    const markerObservable = this.markers.get(marker);
+    if (markerObservable) {
+      markerObservable.subscribe((m: google.maps.Marker) => m.setDraggable(marker?.draggable || false));
+    }
+  }
+
+  public updateIcon(marker: AgmrMarker): void {
+    const markerObservable = this.markers.get(marker);
+    if (markerObservable) {
+      markerObservable.subscribe((m: google.maps.Marker) => m.setIcon(marker?.iconUrl || null));
+    }
+  }
+
+  public updateOpacity(marker: AgmrMarker): void {
+    const markerObservable = this.markers.get(marker);
+    if (markerObservable) {
+      markerObservable.subscribe((m: google.maps.Marker) => m.setOpacity(marker?.opacity || 1));
+    }
+  }
+
+  public updateVisible(marker: AgmrMarker): void {
+    const markerObservable = this.markers.get(marker);
+    if (markerObservable) {
+      markerObservable.subscribe((m: google.maps.Marker) => m.setVisible(marker?.visible || true));
+    }
+  }
+
+  public updateZIndex(marker: AgmrMarker): void {
+    const markerObservable = this.markers.get(marker);
+    if (markerObservable) {
+      markerObservable.subscribe((m: google.maps.Marker) => m.setZIndex(marker?.zIndex || 0));
+    }
+  }
+
+  public updateClickable(marker: AgmrMarker): void {
+    const markerObservable = this.markers.get(marker);
+    if (markerObservable) {
+      markerObservable.subscribe((m: google.maps.Marker) => m.setClickable(marker?.clickable || false));
+    }
+  }
+
+  public updateAnimation(marker: AgmrMarker): void {
+    const markerObservable = this.markers.get(marker);
+    if (markerObservable) {
+      this.convertAnimation(marker.animation)
+        .pipe(
+          first(),
+          switchMap((animation) =>
+            markerObservable.pipe(tap((m: google.maps.Marker) => m.setAnimation(animation || null))),
+          ),
+        )
+        .subscribe();
+    }
+  }
+
   public getNativeMarker(marker: AgmrMarker): Observable<google.maps.Marker | undefined> {
     return new Observable((observer: Observer<google.maps.Marker | undefined>) => {
       const markerObservable = this.markers.get(marker);
@@ -23,6 +158,22 @@ export class MarkerManagerService {
       } else {
         observer.next(undefined);
         observer.complete();
+      }
+    });
+  }
+
+  public createEventObservable<T extends google.maps.MapMouseEvent | void>(
+    eventName: string,
+    marker: AgmrMarker,
+  ): Observable<T> {
+    return new Observable((observer) => {
+      const markerObservable = this.markers.get(marker);
+      if (markerObservable) {
+        markerObservable.subscribe((m) => {
+          m.addListener(eventName, (e: T) => this.zone.run(() => observer.next(e)));
+        });
+      } else {
+        observer.next();
       }
     });
   }
