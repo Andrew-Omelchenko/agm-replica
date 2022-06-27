3e886e6428b3e7422f01bc94277dcee31b20b662
Andrew Omelchenko <andrewom43@gmail.com>
2022-06-27 18:09:34 +0300

    [AGMR-0015] Added markers to test.

--- src/app/shared/modules/agm-replica/directives/agmr-marker.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AgmrMarkerDirective } from './agmr-marker.directive';
-
-describe('AgmrMarkerDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AgmrMarkerDirective();
-    expect(directive).toBeTruthy();
-  });
-});
